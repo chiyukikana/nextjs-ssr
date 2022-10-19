e1baf1d42c814e9c41f749efe5788a2c2bb960fa
chiyukikana <chiyukikana@icloud.com>
2022-10-19 09:40:36 +0800

    update

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,12 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 export default function Post({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const router = useRouter();
-  const { id } = router.query;
+  const router = useRouter()
+  const { id } = router.query
 
   return (
     <div>
@@ -16,28 +16,28 @@ export default function Post({
       <p>Weight: {data.weight}</p>
       <p>
         <Image
-          src={data.sprites.other["official-artwork"].front_default}
+          src={data.sprites.other['official-artwork'].front_default}
           alt="pokemon pic"
           width={400}
           height={400}
         />
       </p>
     </div>
-  );
+  )
 }
 
 type Data = {
-  name: string;
-};
+  name: string
+}
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { id } = context.query;
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  const data: Data = await res.json();
+export const getServerSideProps: GetServerSideProps = async context => {
+  const { id } = context.query
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const data: Data = await res.json()
 
   return {
     props: {
       data,
     },
-  };
-};
+  }
+}
